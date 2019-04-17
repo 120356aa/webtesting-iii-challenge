12aa7ad09b2dad9f120546f09d54ad27dc1fb08f
120356aa <yurlovandrew@gmail.com>
2019-04-16 18:56:57 -0700

    controls should render

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { toBeDisabled } from 'jest-dom';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 
 import Controls from './Controls';
+
+expect.extend({ toBeDisabled });
 
 describe('<Controls />', () => {
   it('should match snap', () => {
     const tree = renderer.create(<Controls />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should render', () => {
+    render(<Controls />);
+    cleanup();
   });
 });
