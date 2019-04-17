736451e37314c32a340d440abd8f9493442f24d1
120356aa <yurlovandrew@gmail.com>
2019-04-16 18:34:13 -0700

    display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Display from './Display'
+
+describe('<Display />', () => {
+  it('should match snap', () => {
+    const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
