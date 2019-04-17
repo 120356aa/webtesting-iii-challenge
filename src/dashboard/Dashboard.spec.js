fac13c8e057cc7ac81aa5bfeeaaf722507af15a8
120356aa <yurlovandrew@gmail.com>
2019-04-16 19:07:39 -0700

    dashboard render test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, cleanup } from 'react-testing-library';
 
 import Dashboard from './Dashboard';
 
@@ -8,4 +9,9 @@ describe('<Dashboard />', () => {
     const tree = renderer.create(<Dashboard />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  it('should render', () => {
+    render(<Dashboard />);
+    cleanup();
+  })
 });
